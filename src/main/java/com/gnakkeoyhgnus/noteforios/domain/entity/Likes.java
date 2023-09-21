8ba97b5bb6f3973f0ca8fc3d3abf68a6d3b4b557
NokchaMat <rkdtjd976@gmail.com>
2023-09-21 17:25:08 +0900

    ## Likes - fetch - Lazy로 변경

--- src/main/java/com/gnakkeoyhgnus/noteforios/domain/entity/Likes.java
+++ src/main/java/com/gnakkeoyhgnus/noteforios/domain/entity/Likes.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -30,11 +31,11 @@ public class Likes {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
   private User user;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "page_share_board_id")
   private PageShareBoard pageShareBoard;
 
