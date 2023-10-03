de441daf34e847c49eb9261450a6c7152d48c608
NokchaMat <rkdtjd976@gmail.com>
2023-10-04 00:03:46 +0900

    ## 채팅 설정 - 웹소켓 사용을 위한 SecurityConfig 설정에서 staticResource는 모두 접근할 수 있도록 함 - 따로 웹소켓 연결 시 preHandler를 통해 jwt토큰을 확인하므로 /ws경로 접근 가능으로 설정 (해당 부분 고민 필요...)

---
+++ src/main/java/com/gnakkeoyhgnus/noteforios/config/StompHandler.java
@@ -0,0 +1,53 @@
+package com.gnakkeoyhgnus.noteforios.config;
+
+import static com.gnakkeoyhgnus.noteforios.jwt.JwtTokenProvider.PREFIX;
+
+import com.gnakkeoyhgnus.noteforios.exception.CustomException;
+import com.gnakkeoyhgnus.noteforios.exception.ErrorCode;
+import com.gnakkeoyhgnus.noteforios.jwt.JwtTokenProvider;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StompHandler implements ChannelInterceptor {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Override
+  public Message<?> preSend(Message<?> message, MessageChannel channel) {
+    log.info("preSend 시작");
+    StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+    if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+      String token = accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
+      if (token != null && token.startsWith(PREFIX)) {
+        log.info("preSend PREFIX 확인");
+        token = token.substring(PREFIX.length());
+        if (jwtTokenProvider.isTokenValid(token)) {
+          log.info("preSend TokenValid");
+          Authentication auth = jwtTokenProvider.getAuthentication(token);
+          Objects.requireNonNull(accessor.getSessionAttributes())
+              .put("user", auth.getName());
+        }
+      } else {
+        log.error("토큰이 없거나 형식이 맞지 않습니다.");
+        throw new CustomException(ErrorCode.PERMISSION_DENIED);
+      }
+
+    }
+
+    return message;
+  }
+}
