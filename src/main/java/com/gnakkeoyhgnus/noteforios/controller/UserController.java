a79281c4c132cfbfe59c040ec5048969fb47a8aa
NokchaMat <rkdtjd976@gmail.com>
2023-10-31 16:28:50 +0900

    ### ProfileImageUpdate - 프로필 이미지 수정 로직 구현 - 유저를 인자로 받은 경우 영속성이 없기 때문에 Repository로 저장

--- src/main/java/com/gnakkeoyhgnus/noteforios/controller/UserController.java
+++ src/main/java/com/gnakkeoyhgnus/noteforios/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -41,7 +42,7 @@ public class UserController {
     String token = userService.signIn(signInForm);
     HttpHeaders headers = new HttpHeaders();
     headers.add(HttpHeaders.AUTHORIZATION, token);
-
+현
     return ResponseEntity.ok().headers(headers).body(null);
   }
 
@@ -58,4 +59,12 @@ public class UserController {
     return ResponseEntity.ok(userService.getProfileByUserId(user, userId));
   }
 
+  @PutMapping("/profile")
+  public ResponseEntity<Void> updateProfileImage(@AuthenticationPrincipal User user,
+      @RequestPart MultipartFile profileImage) {
+
+    userService.updateProfileImage(user, profileImage);
+
+    return ResponseEntity.ok(null);
+  }
 }
